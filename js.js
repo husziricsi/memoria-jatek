ea983736a1ebd31212ec32c43d3c80ae554e972f
RichardHuszar <huszirics10@gmail.com>
2021-02-16 09:55:36 +0100

    Kártya betöltése, műkődése

--- js.js
+++ js.js
@@ -1,9 +1,32 @@
-$(function(){
+$(function () {
 //   $("#kezd").click(init);
-   $("#kezd").click("click",init);
+    $("#kezd").click("click", init);
 });
 
-function init(){
-    
+function init() {
+    $("#jatekter").empty();
+    for (var i = 0; i < 32; i++) {
+        $("#jatekter").append("<div>");
+        $("#jatekter div").eq(i).append("<img>");
+        $("#jatekter div").eq(i).attr({"id": (i + 1)});
+        $("#jatekter img").eq(i).attr({"src": "kartya/hatlap.jpg", "alt": "kep", "id": (i + 1)});
+
+
+    }
+
+    $("#jatekter div").click(lapcsere);
+    $("#jatekter div ").hover(function () {
+        $(this).addClass("kartya");
+    },
+            function () {
+                $(this).removeClass("kartya");
+            });
+}
+
+function lapcsere() {
+    var i = $(this).attr("id");
+    var fajlnev = "kartya/" + (i) + ".png";
+    console.log(fajlnev);
+    $("#jatekter img").eq(i - 1).attr({"src": fajlnev, "alt": "kartyalap"});
 }
 
